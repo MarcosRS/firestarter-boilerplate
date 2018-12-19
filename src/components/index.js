16702155de31ca9753ca65d42b9723a275efe255
Marcos Reynoso <marcos.reynoso@dowjones.com>
2018-12-19 17:21:33 -0400

    add: sass bundling

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, withRouter } from 'react-router-dom';
+import '../stylesheets/global.scss'
 
 class App extends React.Component {
     render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack');
 const liveReloadPlugin = require('webpack-livereload-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: __dirname,
-        filename: './public/assets/bundle',
+        filename: './public/assets/bundle.js',
     },
     devtool: 'source-map',
     resolve: {
@@ -13,21 +14,20 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['react', 'es2015', 'stage-2'],
+                    test: /\.jsx?$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'es2015', 'stage-2'],
+                    },
                 },
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
+                {
+                    test: /\.scss$/,
+                    loader: ExtractTextPlugin.extract({
+                        use: ['css-loader', 'sass-loader'],
+                        fallback: 'style-loader'
+                    })
+                },
         ],
     },
     plugins: [
@@ -38,6 +38,7 @@ module.exports = {
             compress: {
                 warnings: false
             }
-        })
+        }),
+        new ExtractTextPlugin('./public/assets/bundle.css'),
     ],
 };
