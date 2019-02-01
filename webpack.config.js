935563fb14cfefd45dfb27542763a709cc22f7a3
Marcos Reynoso <marcos.reynoso@dowjones.com>
2019-02-01 13:48:24 -0500

    update: package josn and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,21 @@ const webpack = require('webpack');
 const liveReloadPlugin = require('webpack-livereload-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const dev = process.argv.indexOf('-d') !== -1;
+
+let plugins = [
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    }),
+    new ExtractTextPlugin('./public/assets/bundle.css')
+]
+
+if (dev) {
+    plugins.push(new liveReloadPlugin({ appendScriptTag: true }))
+}
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -14,31 +29,21 @@ module.exports = {
     },
     module: {
         rules: [{
-                    test: /\.jsx?$/,
-                    exclude: /(node_modules|bower_components)/,
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['react', 'es2015', 'stage-2'],
-                    },
-                },
-                {
-                    test: /\.scss$/,
-                    loader: ExtractTextPlugin.extract({
-                        use: ['css-loader', 'sass-loader'],
-                        fallback: 'style-loader'
-                    })
-                },
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['react', 'es2015', 'stage-2'],
+            },
+        },
+        {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract({
+                use: ['css-loader', 'sass-loader'],
+                fallback: 'style-loader'
+            })
+        },
         ],
     },
-    plugins: [
-        new liveReloadPlugin({
-            appendScriptTag: true
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new ExtractTextPlugin('./public/assets/bundle.css'),
-    ],
+    plugins: plugins
 };
